8e962ec2f02ade0bc2bed5006c76d30c8f43c4db
Andrew Shvetsov <av-shvesov2015@yandex.ru>
2022-09-06 00:02:39 +0300

    init

---
+++ AccountingSystemOfUniversityClassroomFundAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingSystemOfUniversityClassroomFundAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "<h1> Hello world </h1>";
+        }
+    }
+}
